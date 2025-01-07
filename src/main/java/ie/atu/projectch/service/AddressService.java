0f8e262e00a628f0b3c9ae85d2159dd9607be841
hardycathal <hardyfcathal@gmail.com>
2025-01-07 22:53:33 +0000

    Added AddressService

---
+++ src/main/java/ie/atu/projectch/service/AddressService.java
@@ -0,0 +1,25 @@
+package ie.atu.projectch.service;
+
+import ie.atu.projectch.entity.Address;
+import ie.atu.projectch.repo.AddressRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressService {
+    private final AddressRepo addressRepo;
+
+    public AddressService(AddressRepo addressRepo) {
+        this.addressRepo = addressRepo;
+    }
+
+    public void saveAddress(Address address) {
+        addressRepo.save(address);
+    }
+
+    public List<Address> getAllAddresses() {
+        return addressRepo.findAll();
+    }
+
+}
