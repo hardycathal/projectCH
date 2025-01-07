036cbf27b787973292c56c9b00d272554499f129
hardycathal <hardyfcathal@gmail.com>
2025-01-07 21:37:03 +0000

    Fixed a bug and added get endpoint, Bugfix took circa 5 hours, had to remove lombok annotation and do it manually. Also changed all cases of usser to customer

---
+++ src/main/java/ie/atu/projectch/model/Customer.java
@@ -0,0 +1,81 @@
+package ie.atu.projectch.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String number;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String email, String password, String firstName, String lastName, String number) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
--- src/main/java/ie/atu/projectch/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package ie.atu.projectch.repo;
-
-import ie.atu.projectch.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-}
