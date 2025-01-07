efb676d2849ecc6811bb8bb4ce2c964f502255b8
hardycathal <hardyfcathal@gmail.com>
2025-01-07 15:08:10 +0000

    Created Project

---
+++ src/main/java/ie/atu/projectch/ProjectChApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.projectch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectChApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectChApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/projectch/ProjectChApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectChApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
