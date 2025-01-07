c6fdef4bd6d013ddff1c1725cfe1ed1f9d018979
hardycathal <hardyfcathal@gmail.com>
2025-01-07 22:14:57 +0000

    Added DataLoader to submit dummy data

---
+++ src/main/java/ie/atu/projectch/dummydata/DataLoader.java
@@ -0,0 +1,31 @@
+package ie.atu.projectch.dummydata;
+
+import ie.atu.projectch.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import ie.atu.projectch.model.Customer;
+
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public DataLoader(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Insert your test data here
+        Customer customer1 = new Customer(null, "johndoe@gmail.com", "password123", "John", "Doe", "083 963 2441");
+        Customer customer2 = new Customer(null, "janedoe@gmail.com", "password321", "Jane", "Doe", "086 524 4587");
+        Customer customer3 = new Customer(null, "cathalhardy@gmail.com", "password213", "Cathal", "Hardy", "087 757 5846");
+
+        customerRepo.save(customer1);
+        customerRepo.save(customer2);
+        customerRepo.save(customer3);
+    }
+}
